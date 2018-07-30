e4fee4e19c66e582c53e652a2d382d4bf4f1db3f
sashok15 <olexandrdatsenko@gmail.com>
2018-07-30 18:05:53 +0300

    add gunicorn and simple wsgi

---
+++ hello.py
@@ -0,0 +1,13 @@
+def app(environ, start_response):
+    # бизнес-логика
+    status = '200 OK'
+    headers = [
+                ('Content-Type', 'text/plain')
+    ]
+    
+    # body = [environ.QUERY_STRING.replace('&', '\n')] 
+
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    print(body)
+    start_response(status, headers )
+    return [ body ]
