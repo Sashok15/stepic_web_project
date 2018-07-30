32c7e63675d729b1583935472fa8535e4a5cc1c2
sashok15 <olexandrdatsenko@gmail.com>
2018-07-30 22:56:02 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 def app(environ, start_response):
     status = '200 OK'
     headers = [
-                ('Content-Type', 'text/plain')
+        ('Content-Type', 'text/plain')
     ]
     # body = [environ.QUERY_STRING.replace('&', '\n')] 
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = [i + '\n', for i in environ['QUERY_STRING'].split('&')]
     print(body)
     start_response(status, headers)
     return [ body ]
--- init.sh
+++ init.sh
@@ -1,21 +1,8 @@
-#!/bin/bash
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-gunicorn -c hello.py hello:app
-
-
-# sudo /etc/init.d/mysql start
-
-# sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-# sudo rm -rf /etc/nginx/sites-enabled/default
-# sudo /etc/init.d/nginx restart
-
-# sudo rm /etc/nginx/sites-enabled/default
-# sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-# sudo /etc/init.d/nginx restart
-# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
-
+sudo /etc/init.d/mysql start    
